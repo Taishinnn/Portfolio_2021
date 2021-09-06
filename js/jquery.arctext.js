c2b90f8746f7538e4b24838b446d143d6331c263
Taishinnn <89714602+Taishinnn@users.noreply.github.com>
2021-09-06 11:48:06 +0900

    フェードイン搭載

--- js/jquery.arctext.js
+++ js/jquery.arctext.js
@@ -379,3 +379,20 @@
 	};
 	
 })( jQuery );
+
+// 扇状のテキスト
+
+// フェードイン
+$(document).ready(function(){
+  $('.fuwat').css('visibility','hidden');
+  $(window).scroll(function(){
+  var windowHeight = $(window).height(),
+      topWindow = $(window).scrollTop();
+  $('.fuwat').each(function(){
+    var objectPosition = $(this).offset().top;
+    if(topWindow > objectPosition - windowHeight + 200){
+    $(this).addClass("fuwatAnime");
+    }
+  });
+  });
+});
